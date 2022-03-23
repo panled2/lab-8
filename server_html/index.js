76d80e54aac537e2e3bd7a300333999a630c39b8
panled2 <jacky.pan32@gmail.com>
2022-03-23 15:33:06 -0400

    v1.2

--- server_html/index.js
+++ server_html/index.js
@@ -13,12 +13,70 @@ app.on("request", (req, res) =>{
         body = Buffer.concat(body).toString();
         console.log(`Request body is: ${body}`);
     });
-    if (routeResponseMap[req.url]) {
-        res.end(routeResponseMap[req.url]);
-    } else {
-        res.end("<h1> Welcom </h1>")
+    res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/html",
+    });
+        res.writeHead(httpStatus.StatusCodes.OK, {
+        
+    });
+    let url = req.url;
+    
+    if (url.indexOf(".html") !== -1) {
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/html",
+        });
+        console.log(url)
+        customReadFile(`./views${url}`, res);
+    }
+    else if (url.indexOf(".js") !== -1){
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/javascript",
+        });
+        customReadFile(`./public/js${url}`, res);
+    }
+    
+    else if (url.indexOf(".css") !== -1){
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/CSS",
+        });
+        customReadFile(`./public/css`, res);
+    }
+    
+    else if (url.indexOf(".png") !== -1){
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/png",
+        });
+        customReadFile(`./public/images${url}`, res);
+    }
+
+    else {
+        sendErrorResponse(res)
     }
 });
+
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)) {
+        fs.readFile(file_path, (error, data) => {
+            if (error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+
+    } else {
+        sendErrorResponse(res);
+    }
+};
+const sendErrorResponse = (res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, {
+        "Content-Type": "text/html",
+    });
+    res.write("<h1>file not found </h1>");
+    res.end();
+ }
 
 app.listen(port);
 console.log (`The server is listening on port: ${port}`);
