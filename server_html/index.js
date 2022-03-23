6c0a63f558191e4f4469dd0a158b5d4d5d3b492c
panled2 <jacky.pan32@gmail.com>
2022-03-23 14:18:16 -0400

    v1.1

--- server_html/index.js
+++ server_html/index.js
@@ -1,7 +1,24 @@
-const port =3000,
-http = require("http"),
-httpStatusCodes = require("http-status-codes"),
-router = require("/router"),
-plainTextContentType = {
-    "Content-Type": "text/html"
-}
+const port = 3000;
+const http = require("http");
+const httpStatus = require("http-status-codes");
+const fs = require("fs");
+const app= http.createServer();
+
+app.on("request", (req, res) =>{
+    var body = [];
+    req.on("data", (bodyData) => {
+        body.push(bodyData);
+    });
+    req.on("end", () => {
+        body = Buffer.concat(body).toString();
+        console.log(`Request body is: ${body}`);
+    });
+    if (routeResponseMap[req.url]) {
+        res.end(routeResponseMap[req.url]);
+    } else {
+        res.end("<h1> Welcom </h1>")
+    }
+});
+
+app.listen(port);
+console.log (`The server is listening on port: ${port}`);
